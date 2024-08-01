c0fb40553479f2152806343e20b722d72beec95f
cshezi <cshezi@sybrin.co.za>
2024-08-01 10:53:52 +0200

    feat: new endpoint for saving condtions to arangoDb

--- src/logic.service.ts
+++ src/logic.service.ts
@@ -2,6 +2,7 @@
 import { databaseManager, loggerService } from '.';
 import { unwrap } from '@frmscoe/frms-coe-lib/lib/helpers/unwrap';
 import { type Report } from './interface/report.interface';
+import { type EntityCondition } from '@frmscoe/frms-coe-lib/lib/interfaces';
 
 export const handleGetReportRequestByMsgId = async (msgid: string): Promise<Report | undefined> => {
   try {
@@ -21,5 +22,74 @@ export const handleGetReportRequestByMsgId = async (msgid: string): Promise<Repo
     throw new Error(errorMessage.message);
   } finally {
     loggerService.log('Completed handling get report by message id');
+  }
+};
+
+export const handlePostConditionEntity = async (condition: EntityCondition): Promise<EntityCondition[] | Record<string, unknown>> => {
+  try {
+    loggerService.log(`Started handling post request of entity condition executed by ${condition.usr}.`);
+
+    const alreadyExistingCondition = (await databaseManager.getConditionsByEntity(condition.ntty)) as EntityCondition[];
+
+    const { _id: condId } = (await databaseManager.saveCondition({ ...condition, creDtTm: new Date().toISOString() })) as { _id: string };
+
+    const alreadyExistingEntity = (await databaseManager.getEntity(condition.ntty)) as Array<{ _id: string }>;
+
+    let entityId = '';
+
+    if (!alreadyExistingEntity) {
+      if (condition.forceCret) {
+        const entityIdentifier = `${condition.ntty.Id + condition.ntty.SchmeNm.Prtry}`;
+        const nowDateTime = new Date().toISOString();
+        try {
+          entityId = ((await databaseManager.saveEntity(entityIdentifier, nowDateTime)) as { _id: string })._id;
+        } catch (err) {
+          throw Error((err as { message: string }).message);
+        }
+        loggerService.log('New entity was added after not being found while forceCret was set to true');
+      } else {
+        throw Error('Error: entity was not found and we could not create one because forceCret is set to false');
+      }
+    } else {
+      if (alreadyExistingEntity && alreadyExistingEntity.length > 1) {
+        const message = `${alreadyExistingEntity.length} entities were found with the matching id`;
+        loggerService.error(message);
+        throw Error(message);
+      }
+      entityId = alreadyExistingEntity[0]._id;
+    }
+
+    switch (condition.prsptv) {
+      case 'both':
+        await Promise.all([
+          databaseManager.governedAsCreditorBy(condId, entityId, condition),
+          databaseManager.governedAsDebtorBy(condId, entityId, condition),
+        ]);
+        break;
+      case 'debtor':
+        await databaseManager.governedAsDebtorBy(condId, entityId, condition);
+        break;
+      case 'creditor':
+        await databaseManager.governedAsCreditorBy(condId, entityId, condition);
+        break;
+    }
+
+    if (alreadyExistingCondition && alreadyExistingCondition.length > -1) {
+      const message = `${alreadyExistingCondition.length} conditions already exist for the entity`;
+      loggerService.warn(message);
+      return {
+        message,
+        condition: alreadyExistingCondition,
+      };
+    }
+
+    return {
+      message: 'New condtions was saved successfully.',
+      condition,
+    };
+  } catch (error) {
+    const errorMessage = error as { message: string };
+    loggerService.log(`Failed posting condition for entity with error message: ${errorMessage.message}`);
+    throw new Error(errorMessage.message);
   }
 };
--- src/router.ts
+++ src/router.ts
@@ -1,12 +1,13 @@
 // SPDX-License-Identifier: Apache-2.0
 import SetOptions from './utils/schema-utils';
-import { handleHealthCheck, ReportRequestHandler } from './app.controller';
+import { handleHealthCheck, POSTConditionHandler, ReportRequestHandler } from './app.controller';
 import { type FastifyInstance } from 'fastify';
 
 async function Routes(fastify: FastifyInstance, options: unknown): Promise<void> {
   fastify.get('/', handleHealthCheck);
   fastify.get('/health', handleHealthCheck);
   fastify.get('/v1/admin/reports/getreportbymsgid', SetOptions(ReportRequestHandler, 'messageIDSchema'));
+  fastify.post('/v1/admin/event-flow-control/entity', POSTConditionHandler);
 }
 
 export default Routes;

7db08d1b054941c9d303b09cca2d06bbb38babbb
cshezi <cshezi@sybrin.co.za>
2024-08-08 13:04:56 +0200

    feat: added new route of saving conditions for account

--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: Apache-2.0
 import { type FastifyInstance } from 'fastify';
-import { POSTConditionHandlerEntity, ReportRequestHandler, handleHealthCheck } from './app.controller';
+import { POSTConditionHandlerEntity, POSTConditionHandlerAccount, ReportRequestHandler, handleHealthCheck } from './app.controller';
 import { SetOptionsBody, SetOptionsParams } from './utils/schema-utils';
 
 async function Routes(fastify: FastifyInstance): Promise<void> {
@@ -8,6 +8,7 @@ async function Routes(fastify: FastifyInstance): Promise<void> {
   fastify.get('/health', handleHealthCheck);
   fastify.get('/v1/admin/reports/getreportbymsgid', SetOptionsParams(ReportRequestHandler, 'messageIDSchema'));
   fastify.post('/v1/admin/event-flow-control/entity', SetOptionsBody(POSTConditionHandlerEntity, 'entityConditionSchema'));
+  fastify.post('/v1/admin/event-flow-control/account', POSTConditionHandlerAccount);
 }
 
 export default Routes;

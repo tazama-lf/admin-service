141ec36b7f6c22ba8cd927b0d6f1e24e6756e8c4
cshezi <cshezi@sybrin.co.za>
2024-08-08 13:19:11 +0200

    feat: assigned the schema to validate the route

--- src/router.ts
+++ src/router.ts
@@ -8,7 +8,7 @@ async function Routes(fastify: FastifyInstance): Promise<void> {
   fastify.get('/health', handleHealthCheck);
   fastify.get('/v1/admin/reports/getreportbymsgid', SetOptionsParams(ReportRequestHandler, 'messageIDSchema'));
   fastify.post('/v1/admin/event-flow-control/entity', SetOptionsBody(POSTConditionHandlerEntity, 'entityConditionSchema'));
-  fastify.post('/v1/admin/event-flow-control/account', POSTConditionHandlerAccount);
+  fastify.post('/v1/admin/event-flow-control/account', SetOptionsBody(POSTConditionHandlerAccount, 'accountConditionSchema'));
 }
 
 export default Routes;

21714b679d11fc924c937cbe996df9323596a1e9
cshezi <cshezi@sybrin.co.za>
2024-09-26 04:21:11 +0200

    refactor: helper function refactorization

--- src/utils/schema-utils.ts
+++ src/utils/schema-utils.ts
@@ -7,43 +7,17 @@ import { tokenHandler } from '../auth/authHandler';
 
 type preHandler = (request: FastifyRequest, reply: FastifyReply) => Promise<void>;
 
-export const SetOptionsBody = (
-  handler: RouteHandlerMethod,
-  bodySchemaName: string,
-): { handler: RouteHandlerMethod; schema: FastifySchema } => {
-  const schema: FastifySchema = { body: { $ref: `${bodySchemaName}#` } };
-
-  return {
-    handler,
-    schema,
-  };
-};
-
-export const SetOptionsParams = (
-  handler: RouteHandlerMethod,
-  paramSchemaName: string,
-  claim: string,
-): { preHandler?: preHandler; handler: RouteHandlerMethod; schema: FastifySchema } => {
-  loggerService.debug(`Authentication is ${configuration.authentication ? 'ENABLED' : 'DISABLED'} for ${handler.name}`);
-  const preHandler = configuration.authentication ? tokenHandler(claim) : undefined;
-  const schema: FastifySchema = { querystring: { $ref: `${paramSchemaName}#` } };
-
-  return {
-    preHandler,
-    handler,
-    schema,
-  };
-};
-
 export const SetOptionsBodyAndParams = (
   handler: RouteHandlerMethod,
-  bodySchemaName: string,
-  paramSchemaName: string,
   claim: string,
+  bodySchemaName?: string,
+  paramSchemaName?: string,
 ): { preHandler?: preHandler; handler: RouteHandlerMethod; schema: FastifySchema } => {
   loggerService.debug(`Authentication is ${configuration.authentication ? 'ENABLED' : 'DISABLED'} for ${handler.name}`);
   const preHandler = configuration.authentication ? tokenHandler(claim) : undefined;
-  const schema: FastifySchema = { querystring: { $ref: `${paramSchemaName}#` }, body: { $ref: `${bodySchemaName}#` } };
+  const querystring = paramSchemaName ? { querystring: { $ref: `${paramSchemaName}#` } } : undefined;
+  const body = bodySchemaName ? { body: { $ref: `${bodySchemaName}#` } } : undefined;
+  const schema: FastifySchema = { ...querystring, ...body };
 
   return {
     preHandler,

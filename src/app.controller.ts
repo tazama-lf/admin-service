3a2ce150ed8b16cd064b3545eed68ab50dccede1
ReebaPaysys <reeba.siddiqui@paysyslabs.com>
2026-04-28 13:01:41 +0500

    fix: comment out transactionType and tenantId in getPayloadByTransactionTypeHandler

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1487,8 +1487,8 @@ export const getPayloadByTransactionTypeHandler = async (req: FastifyRequest, re
 
     reply.status(200).send({
       success: true,
-      transactionType,
-      tenantId,
+      // transactionType, // commented in CFP-321
+      // tenantId,
       payload,
     });
   } catch (error: unknown) {
